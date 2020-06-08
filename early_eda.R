20edd83ff61447ac962e2c83dbc86d0ca0017c34
“saibib”​ <thesaimun@gmail.com>
2020-06-08 14:44:15 -0400

    can read in now

--- early_eda.R
+++ early_eda.R
@@ -1,9 +1,14 @@
-df = CFD_CARS_EMS_DATA_121616TO51320
+df =fread("./data/original/CFD_CARS_EMS_DATA_121616TO51320.csv", na.strings = c(""), colClasses = rep("character", 78))
+df = read.csv("./data/original/CFD_CARS_EMS_DATA_121616TO51320.csv", na.strings = "")
+dt[,.N,by=Incident.Complaint.Reported.By.Dispatch..eDispatch.01.]
+dt = as.data.table(df)
+
+(df$Incident.Complaint.Reported.By.Dispatch..eDispatch.01.)
 nas_in_column = colSums(is.na(df))
 nas_in_column
 class(nas_in_column)
 nas_in_column[1]
-not_all_nas = nas_in_column[nas_in_column<max(nas_in_column)]
+not_all_nas = nas_in_column[nas_in_column < max(nas_in_column)]
 not_all_nas
 class(df)
 df$`Incident Date`
