3029210d58fbd5513a3abe84e7aeb5bee11d7d8c
“saibib”​ <thesaimun@gmail.com>
2020-06-08 23:11:59 -0400

    added some plots

--- early_eda.R
+++ early_eda.R
@@ -1,7 +1,26 @@
-df =fread("./data/original/CFD_CARS_EMS_DATA_121616TO51320.csv", na.strings = c(""), colClasses = rep("character", 78))
-df = read.csv("./data/original/CFD_CARS_EMS_DATA_121616TO51320.csv", na.strings = "")
-dt[,.N,by=Incident.Complaint.Reported.By.Dispatch..eDispatch.01.]
+library(data.table)
+library(dplyr)
+
+df =read.csv("./data/original/CFD_CARS_EMS_DATA_121616TO51320.csv", na.strings = " ")
 dt = as.data.table(df)
+glimpse(dt)
+#creates a key by incident number and drops all duplicates
+setkeyv(dt, c("Response.Incident.Number..eResponse.03."))
+uniqdat <- subset(unique(dt))
+
+#sorts the most common incident types and plots the top_incidents most common
+incidents = uniqdat %>% count(incident_type = Incident.Complaint.Reported.By.Dispatch..eDispatch.01., sort =T)
+incidents = incidents[-1]
+top_incidents = 7
+incidents$incident_type = factor(incidents$incident_type)
+x = factor(rep(incidents[1:top_incidents]$incident_type, times = incidents[1:top_incidents]$n))
+others = factor(rep("Other", times =nrow(uniqdat)-length(x)))
+f = factor(c(levels(x)[x], levels(others)[others]))
+pie(table(f), main = 'Pie Chart of Incidents')
+#histogram/boxplot of patient age distribution, two big modes around 60 and 20, the former being larger
+summary(uniqdat$`Patient.Age..ePatient.15.`)
+boxplot(uniqdat$`Patient.Age..ePatient.15.`, xlab= 'Patient Age', main ='Histogram of Patient Age')
+hist(uniqdat$`Patient.Age..ePatient.15.`,xlab='Patient Age', main = 'Histogram of Patient Age')
 
 (df$Incident.Complaint.Reported.By.Dispatch..eDispatch.01.)
 nas_in_column = colSums(is.na(df))
