5430e2ef12142aafdaf462be4776104ee48e8dea
Ellen Graham <eg5se@virginia.edu>
2020-06-10 15:22:21 -0400

    Started mapping showing neighborhoods

---
+++ src/neighborhood_examining.R
@@ -0,0 +1,73 @@
+library(sf)
+library(here)
+library(ggplot2)
+library(tigris)
+library(leaflet)
+library(tidycensus)
+library(tidyr)
+library(dplyr)
+library(RColorBrewer)
+
+census_api_key(census_key)
+
+# download.file("http://widget.charlottesville.org/gis/zip_download/planning_area.zip", here("planning_area.zip"))
+# unzip(here("planning_area.zip"), exdir = here("planning_area"))
+
+planning_area <- st_read(here("planning_area/planning_area_06_04_2020.shp"))
+
+nrow(planning_area)
+
+# checking overlap with block groups
+
+cville_bgs <- block_groups(state = "Virginia", county = c("Charlottesville"), cb = TRUE) %>%
+  st_as_sf() %>%
+  st_transform(crs = 4326)
+#
+# planning_area %>%
+#   ggplot() +
+#   geom_sf(fill = NA, color = "red", size = 1) +
+#   geom_sf(data = cville_bgs, fill = NA, color = "blue") +
+#   theme_minimal()
+
+v18 <- load_variables(2018, "acs5", cache = TRUE)
+
+
+
+acs_data <- get_acs("block group",
+                      variables = c(total_pop = "B02001_001", black_alone = "B02001_003", white_alone = "B02001_002",
+                                    median_hhi = "B19013_001"),
+                      state = "VA",
+                      county = "Charlottesville city",
+                      year = 2018)
+
+acs_data_sp <- acs_data %>%
+  select(-moe) %>% # only plotting estimates
+  pivot_wider(names_from = variable, values_from = estimate) %>%
+  left_join(cville_bgs, ., by = "GEOID") %>%
+  st_transform(4326) %>%
+  mutate(black_percent = black_alone / total_pop)
+
+
+pal <- colorBin(brewer.pal(5, "Greens"), acs_data_sp$black_percent, 5, pretty = TRUE)
+
+planning_area %>%
+  st_transform(crs = 4326) %>%
+  leaflet() %>%
+  addTiles() %>%
+  addPolygons(data = acs_data_sp,
+              fillColor = ~pal(black_percent),
+              fillOpacity = 0.8,
+              color = "#666",
+              weight = 1) %>%
+  addPolygons(color = "#444",
+              opacity = 0,
+              weight = 1.2,
+              fillOpacity = 0 ,
+              label = ~NAME) %>%
+  addLegend(data = acs_data_sp,
+            pal = pal,
+            values = ~black_percent,
+            title = "Proportion Black")
+
+
+
