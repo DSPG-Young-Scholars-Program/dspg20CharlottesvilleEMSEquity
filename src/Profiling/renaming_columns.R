93df47428e62efce0fe18ab1955712cbcca722ae
Ellen Graham <eg5se@virginia.edu>
2020-06-15 18:18:36 -0400

    added comments

--- src/Profiling/renaming_columns.R
+++ src/Profiling/renaming_columns.R
@@ -7,12 +7,12 @@ charlottesville <- readxl::read_xlsx(here("data/original/CFD_CARS_EMS_DATA_12161
 
 
 albemarle %>%
-  rename_with(~tolower(gsub(r"(\.\..*)", "", .x))) %>%
-  rename_with(~gsub(r"(\.)", "_", .x)) %>%
+  rename_with(~tolower(gsub(r"(\.\..*)", "", .x))) %>% # remove code after variable names
+  rename_with(~gsub(r"(\.)", "_", .x)) %>% # change periods to underscores
   names()
 
 
 charlottesville %>%
-  rename_with(~tolower(gsub(r"( \(.*)", "", .x))) %>%
-  rename_with(~gsub(" ", "_", .x)) %>%
+  rename_with(~tolower(gsub(r"( \(.*)", "", .x))) %>% # remove code after variable names
+  rename_with(~gsub(" ", "_", .x)) %>% # change spaces to underscores
   names()
