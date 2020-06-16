dd6a30e513e446ba4ca303a3184b0824fa2e42d1
Finn Roberts <robe2037@umn.edu>
2020-06-16 10:16:03 -0400

    added compare_dates function

--- src/Profiling/profiling_finn.R
+++ src/Profiling/profiling_finn.R
@@ -1,6 +1,7 @@
 
 library(data.table)
 library(naniar)
+library(visdat)
 #library(stringr)
 library(dplyr)
 library(here)
@@ -10,9 +11,6 @@ library(ggplot2)
 ## Read in data for county and city
 alb_data <- read.csv(here("data", "original", "Data4UVA.csv"), fileEncoding="UTF-16LE", sep = ",", na.strings = "")
 cville_data <- readxl::read_xlsx(here("data", "original", "CFD_CARS_EMS_DATA_121616TO60920.xlsx"))
-
-#glimpse(alb_data)
-#glimpse(cville_data)
 
 ## Standardize column names for each dataset
 alb_data <- alb_data %>%
@@ -47,9 +45,21 @@ as.character(alb_data$incident_date)[parse_fails]
 ## It looks like these rows are just shifted to the right or left. As if there was a missing comma on the end of the line or something.
 error_rows <- alb_data[parse_fails,]
 
+## Error rows either completely populated with nonsense or filled with nearly all NAs
+# vis_miss(error_rows)
+
+## Quick check to see if these are consecutive entries.
+values <- vector()
+
+for (i in seq_along(parse_fails)) {
+  val <- parse_fails[i+1] - parse_fails[i]
+  values <- append(values, val)
+}
+
+values ## Looks like there is a long sequence of errors, but that's not the only thing that's going on.
 
 
-
+# ------------------------------------------------------------------------------------------------------------------------------
 
 ## All date time columns
 cville_datetimes <- cville_data %>% select(incident_date, 
@@ -58,20 +68,49 @@ cville_datetimes <- cville_data %>% select(incident_date,
                                            cardiac_arrest_initial_cpr_date_time, 
                                            cardiac_arrest_rosc_date_time)
 
-## Confirm all datetime columns have same intial date as overall incident date
-which(as.Date(cville_datetimes$incident_date) != as.Date(cville_datetimes$incident_psap_call_date_time)) ## Good
-which(as.Date(cville_datetimes$incident_date) != as.Date(cville_datetimes$cardiac_arrest_date_time)) ## Some errors
-which(as.Date(cville_datetimes$incident_date) != as.Date(cville_datetimes$cardiac_arrest_initial_cpr_date_time)) ## Good
-which(as.Date(cville_datetimes$incident_date) != as.Date(cville_datetimes$cardiac_arrest_rosc_date_time)) ## Good
+## Basic function to ensure dates are the same for all combinations of datetime columns
+compare_dates <- function(data, cols, indices = FALSE) {
+  
+  results <- c() ## Empty vector for storing indices
+  combinations <- combn(cols, 2) ## Get all possible combinations of columns to compare
+  
+  ## Iterate through combinations and identify instances where dates are inconsistent
+  for (comb in seq(1:ncol(combinations))) {
+    var1 <- combinations[1, comb]
+    var2 <- combinations[2, comb]
+    
+    ref_dates <- as.Date(pull(data, var1))
+    comp_dates <- as.Date(pull(data, var2))
+    
+    errs <- which(ref_dates != comp_dates)
+    
+    results <- c(results, errs)
+  }
+  
+  ## Indices argument indicates whether to return error locations or values
+  if (indices == FALSE) {
+    return(data[unique(results),])
+  } else {
+    return(unique(results))
+  }
+  
+}
 
-## Make sure cardiac datetimes are consistent
-which(as.Date(cville_datetimes$cardiac_arrest_date_time) != as.Date(cville_datetimes$cardiac_arrest_initial_cpr_date_time)) ## Good
-which(as.Date(cville_datetimes$cardiac_arrest_date_time) != as.Date(cville_datetimes$cardiac_arrest_rosc_date_time)) ## Good
+## Datetime columns for comparison
+comp_cols <- c("incident_date",
+               "incident_psap_call_date_time",
+               "cardiac_arrest_date_time",
+               "cardiac_arrest_initial_cpr_date_time",
+               "cardiac_arrest_rosc_date_time")
 
-## Cases where there is no cardiac arrest time but there is a CPR time
-which(is.na(cville_datetimes$cardiac_arrest_date_time) & !is.na(cville_datetimes$cardiac_arrest_initial_cpr_date_time)) ## Good
-which(is.na(cville_datetimes$cardiac_arrest_date_time) & !is.na(cville_datetimes$cardiac_arrest_rosc_date_time)) ## Good
-which(is.na(cville_datetimes$cardiac_arrest_initial_cpr_date_time) & !is.na(cville_datetimes$cardiac_arrest_rosc_date_time)) ## Good
+## ID inconsistent dates
+date_errs <- compare_dates(cville_datetimes, comp_cols)
+date_errs
+
+
+
+
+
 
 ## Next confirm that times make sense - not just dates
 
@@ -84,6 +123,7 @@ cville_datetimes %>%
 
 
 
+#-----------------
 
 ## County data - won't work until the file errors are fixed...
 alb_datetimes <- alb_data %>% select(incident_date, 
