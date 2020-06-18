e1ea45c9178c97aa8e4e13eaf5655c62262658d6
Ellen Graham <eg5se@virginia.edu>
2020-06-18 11:14:14 -0400

    updated here calls to proper format

--- src/Profiling/joining_albemarle_charlottesville.R
+++ src/Profiling/joining_albemarle_charlottesville.R
@@ -3,13 +3,14 @@ library(here)
 library(purrr)
 
 
-albemarle <- read.csv(here("data/original/Data4UVA.csv"),
+albemarle <- read.csv(here("data","original","Data4UVA.csv"),
                        fileEncoding="UTF-16LE",
-                       sep = ",", na.strings = "") %>%
+                       sep = ",", na.strings = "",
+                      colClasses = "character") %>%
   rename_with(~tolower(gsub(r"(\.\..*)", "", .x))) %>% # remove code after variable names
   rename_with(~gsub(r"(\.)", "_", .x)) %>%  # change periods to underscores
   mutate(incident_date = lubridate::ymd(incident_date))
-charlottesville <- readxl::read_xlsx(here("data/original/CFD_CARS_EMS_DATA_121616TO60920.xlsx"), 1) %>%
+charlottesville <- readxl::read_xlsx(here("data","original","CFD_CARS_EMS_DATA_121616TO60920.xlsx"), 1) %>%
   rename_with(~tolower(gsub(r"( +\(.*)", "", .x))) %>% # remove code after variable names
   rename_with(~gsub(" ", "_", .x)) %>% # change spaces to underscores
   mutate(across(c(1:4, 6:83), as.character))
@@ -44,7 +45,7 @@ albemarle <- albemarle %>%
 # due to the data in the wrong columns in albemarle, I'm going to set charlottesville to all characters
 
 ems_full <- bind_rows(mutate(albemarle, source = "albemarle"),
-                      mutate(charlottesville, source = "charlottesville"))
+                      mutate(charlottesville, source = "charlottesville")) # tag on source variable
 
 # check it worked:
 # nrow(ems_full) == nrow(albemarle) + nrow(charlottesville)
