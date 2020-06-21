4d539c19fff9c9be93213f32a7624e40f27c2db9
“saibib”​ <thesaimun@gmail.com>
2020-06-21 13:43:14 -0400

    removed old file

--- src/Profiling/consistency_check.R
+++ src/Profiling/consistency_check.R
@@ -1,9 +1,9 @@
 #todo
-#check date of incident_psap_call_date_time vs. incident date
 #check if cardiac_arrest_date_time is after incident date time and after "cardiac_arrest_initial_cpr_date_time"+
 #"cardiac_arrest_rosc_date_time" + destination_cardiac_arrest_team_activation_date_time
 
 library(dplyr)
+library(data.table)
 
 albemarle <- read.csv2("./data/original/Data4UVA.csv", fileEncoding="UTF-16LE", sep = ",", na.strings = "")
 charlottesville <- readxl::read_xlsx("./data/original/CFD_CARS_EMS_DATA_121616TO60920.xlsx", 1)
@@ -21,31 +21,54 @@ chr <- charlottesville %>%
 
 #finds different cols in datasets, patient_respiratory_effort_list_ vs. patient_respiratory_effort_list and
 #total_unit_response_time vs. incident_unit_notified_by_dispatch_to_unit_arrived_on_scene_in_minutes
-cols = !colnames(chr)==colnames(alb)
-colnames(chr[,cols])
-colnames(alb[,cols])
+setdiff(union(colnames(chr), colnames(alb)),
+        intersect(colnames(chr), colnames(alb)))
 
 #finding the number and indices of entries where age range is na but patient age is not (or vice versa)
 find_age_mismatch = function(df){
-  which(!is.na(df$patient_age_range_in_years)==is.na(df$patient_age))
+  df[which(!is.na(df$patient_age_range_in_years)==is.na(df$patient_age)),]
 }
-alb_ind = find_age_mismatch(alb) # 33427  and 49906  seem to have a lot of misentries
-View(alb[alb_ind,])
-chr_ind = find_age_mismatch(chr) # 10855 1yr old whose been drunk for 3 days? lots of <1 year olds
-View(chr[chr_ind,])
+alb_age = find_age_mismatch(alb) # 33427  and 49906  seem to have a lot of misentries
+View(alb_age)
+chr_age = find_age_mismatch(chr) # 10855 1yr old whose been drunk for 3 days? lots of <1 year olds
+View(chr_age)
 
 
 find_gps_mismatch = function(df){
-  which(!is.na(df$scene_gps_latitude)==is.na(df$scene_gps_longitude))
+  df[which(!is.na(df$scene_gps_latitude)==is.na(df$scene_gps_longitude)),]
 }
 #incident number seems to actually be call time, incident date is some non date-numeric (MAYBE TIME TO SCENE)
 #lats are things the emts did it seems, longs are NA, most other columns are NAs
-a_inds = find_gps_mismatch(alb)
-View(alb[a_inds,])
+alb_gps = find_gps_mismatch(alb)
+View(a_inds)
 # not the case for chr dataset
-c_inds = find_gps_mismatch(chr)
-View(chr[c_inds,])
+chr_gps = find_gps_mismatch(chr)
+View(c_inds)
 
+# check if incident time is ever less than time for unit to get to site, results don't fit intuition, maybe total unit
+#time is differently calculated than we thought for chr
+find_time_mismatch = function(df){
+  if ("total_unit_response_time" %chin% colnames(df)){
+    df[which(df[,"incident_dispatch_notified_to_unit_arrived_on_scene_in_minutes"]<df[,"total_unit_response_time"]),]
+  }
+  else
+    df[which(df[,"incident_dispatch_notified_to_unit_arrived_on_scene_in_minutes"]<df[,"incident_unit_notified_by_dispatch_to_unit_arrived_on_scene_in_minutes"]),]
+}
+alb_time = find_time_mismatch(alb)
+View(alb_time)
+#rows 1210 and 1211 are nearly identical except patient is complaining for 1 day vs 5 days
+chr_time = find_time_mismatch(chr)
+View(chr_time)
 
+find_date_mismatch = function(df){
+  df[which(!as.Date(df$incident_psap_call_date_time, "%Y-%m-%d") == as.Date(df$incident_date)),]
+}
+#none :)
+chr_date = find_date_mismatch(chr)
+View(chr_date)
+#can't do yet, b/c of weird entries dont think its worth the trouble to check the rows w/o weird entries atm
+# alb_date = find_date_mismatch(alb)
+# View(alb_date)
 
-
+na.exclude(chr$cardiac_arrest_date_time<chr$cardiac_arrest_initial_cpr_date_time)
+chr[which(!is.na(chr$cardiac_arrest_date_time)),][which(!as.Date(chr$incident_psap_call_date_time, "%Y-%m-%d") < as.Date(chr$cardiac_arrest_date_time)),]
