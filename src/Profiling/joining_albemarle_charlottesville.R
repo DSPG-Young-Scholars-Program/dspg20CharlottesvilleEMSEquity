7bd8c714723865b7958a6dbcf6f5fb9d5368f601
Finn Roberts <robe2037@umn.edu>
2020-06-30 10:51:34 -0400

    Dropping relevant columns during data read-in

--- src/Profiling/joining_albemarle_charlottesville.R
+++ src/Profiling/joining_albemarle_charlottesville.R
@@ -1,7 +1,38 @@
+
 library(dplyr)
 library(here)
 library(purrr)
 
+## Columns mutually selected to be removed based on irrelevance or data quality issues
+drop_cols <- c("cad_crew_member_full_name_and_level_list",
+               "cardiac_arrest_etiology_with_code",
+               "cardiac_arrest_initial_cpr_date_time",
+               "cardiac_arrest_indications_resuscitation_attempted_by_ems_with_code_list",
+               "cardiac_arrest_rosc_date_time",
+               "cardiac_arrest_who_initiated_cpr_with_code",
+               "cardiac_arrest_witnessed_by_list",
+               "destination_cardiac_arrest_team_activation_date_time",
+               "incident_type",
+               "injury_cause_of_injury",
+               "injury_cause_of_injury_description_list",
+               "injury_mechanism_of_injury_list",
+               "medication_given_description_and_rxcui_code",
+               "patient_advance_directives_list",
+               "patient_cincinnati_stroke_scale_used",
+               "patient_environmental_food_allergies_list",
+               "patient_head_assessment_exam_details",
+               "patient_initial_stroke_scale_type",
+               "patient_last_oral_intake_date_time",
+               "patient_medical_history_obtained_from_list",
+               "patient_medication_allergies_and_type_list",
+               "patient_medication_given_descriptions_list",
+               "patient_mental_status_assessment_exam_details",
+               "patient_neurological_assessment_exam_details",
+               "patient_respiratory_effort_list",
+               "patient_skin_assessment_exam_details",
+               "patient_weight_actual_or_estimate_pounds",
+               "vitals_cardiac_rhythm_ecg_findings_list",
+               "vitals_level_of_responsiveness_avpu")
 
 albemarle <- readxl::read_xlsx(here("data","original","Data4UVA.xlsx"), 1, col_types = c(rep("text", 4), "date", rep("text", 78))) %>%
   rename_with(~tolower(gsub(r"( +\(.*)", "", .x))) %>% # remove code after variable names
@@ -15,7 +46,7 @@ charlottesville <- readxl::read_xlsx(here("data","original","CFD_CARS_EMS_DATA_1
 albemarle <- albemarle %>%
   rename(total_unit_response_time = incident_unit_notified_by_dispatch_to_unit_arrived_on_scene_in_minutes)
 
-
+## Combine data
 ems_full <- bind_rows(mutate(albemarle, source = "albemarle"),
                       mutate(charlottesville, source = "charlottesville")) # tag on source variable
 
