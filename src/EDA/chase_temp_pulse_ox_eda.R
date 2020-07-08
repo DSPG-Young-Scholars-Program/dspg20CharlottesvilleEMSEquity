e04ecf4709a873fafc15af6f1e3e107c01dc802b
Chase Dawson <scd3dz@virginia.edu>
2020-07-08 11:27:05 -0400

    initial changes to pulse ox eda

--- src/EDA/chase_temp_pulse_ox_eda.R
+++ src/EDA/chase_temp_pulse_ox_eda.R
@@ -157,7 +157,7 @@ tmp <- ems %>%
 
 tmp %>%
   ggplot(., aes(x = incident_month, y = n, fill = dispatch_complaint_potenial_covid)) +
-    geom_bar(stat = "identity", position = "identity") +
+    geom_bar(stat = "identity", position = "fill") +
     labs(title = "City of Charlottesville Incidents in 2019",
          y = "Number of Incidents") +
     theme(axis.title.x = element_blank()) +
@@ -172,10 +172,84 @@ tmp <- ems %>%
 
 tmp %>%
   ggplot(., aes(x = incident_month, y = n, fill = dispatch_complaint_potenial_covid)) +
-  geom_bar(stat = "identity", position = "stack") +
+  geom_bar(stat = "identity", position = "fill") +
 
   labs(title = "City of Charlottesville Incidents in 2020",
        y = "Number of Incidents",
        x = "Month of Incident") +
   theme(axis.title.x = element_blank()) +
   scale_x_continuous(breaks = 1:6, labels = months[1:6])
+
+# looking at provider primary impression
+head(ems$situation_provider_primary_impression_code_and_description)
+levels(as.factor(ems$situation_provider_primary_impression_code_and_description))
+sum(is.na(ems$situation_provider_primary_impression_code_and_description)) # 12445
+related_to_covid <- c("")
+sum(is.na(ems$situation_primary_complaint_statement_list))
+
+ems %>%
+  filter(situation_provider_primary_impression_code_and_description == "Infectious - Coronavirus (B97.21)") %>%
+  count() # 13
+
+ems %>%
+  filter(situation_provider_secondary_impression_description_and_code == "Infectious - Coronavirus (B97.21)") %>%
+  count() # 14
+
+levels(as.factor(ems$situation_provider_secondary_impression_description_and_code))
+
+# plot covid cases
+covid <- ems %>%
+  filter(situation_provider_primary_impression_code_and_description == "Infectious - Coronavirus (B97.21)" |
+           situation_provider_secondary_impression_description_and_code == "Infectious - Coronavirus (B97.21)")
+
+# convert to lower case
+ems$situation_primary_complaint_statement_list <- tolower(ems$situation_primary_complaint_statement_list)
+ems$situation_secondary_complaint_statement_list <- tolower(ems$situation_secondary_complaint_statement_list)
+
+head(covid$situation_primary_complaint_statement_list)
+levels(as.factor(covid$situation_primary_complaint_statement_list))
+covid_symptoms <- c("altered mental status", "chills", "shaking", "cough", "fever", "headache",
+                    "nausea", "shortness of breath", "throat pain", "vomiting", "difficulty breathing")
+pattern <- c("altered mental status|chills|shaking|cough|fever|headache|nausea|shortness of breath|throat pain|vomiting|difficulty breathing")
+
+potential_covid <- ems[str_detect(ems$situation_primary_complaint_statement_list, pattern), ]
+covid2020 <- potential_covid %>%
+  filter(ymd(incident_date) > "2020-01-01")
+hist(covid2020$incident_date, breaks = 20)
+
+ems$potential_covid <- (str_detect(ems$situation_primary_complaint_statement_list, pattern))
+
+tmp <- ems %>%
+  filter(ymd(incident_date) > "2020-01-01") %>%
+  filter(!is.na(potential_covid)) %>%
+  group_by(incident_month, potential_covid) %>%
+  count()
+
+tmp %>%
+  ggplot(., aes(x = incident_month, y = n, fill = potential_covid)) +
+  geom_bar(stat = "identity", position = "fill") +
+  labs(title = "City of Charlottesville Incidents in 2020",
+       y = "Number of Incidents") +
+  theme(axis.title.x = element_blank()) +
+  scale_x_continuous(breaks = 1:6, labels = months[1:6])
+
+plot_data <- ems %>%
+  group_by(mo_yr, potential_covid) %>%
+  count()
+
+plot_data %>%
+  filter(potential_covid) %>%
+  ggplot(., aes(x = mo_yr, y = n)) +
+  geom_line(color = "red") +
+  geom_point(stat = "identity")
+
+# create month year variable
+ems <- ems %>%
+  mutate(incident_date = ymd(incident_date),
+         yr = as.factor(year(incident_date)),
+         mo = as.factor(month(incident_date)),
+         dy = as.character(day(incident_date)),
+         mo_yr = dmy(paste0("01-", mo, "-", yr)))
+
+# look at covid-like symptoms by race
+ems
