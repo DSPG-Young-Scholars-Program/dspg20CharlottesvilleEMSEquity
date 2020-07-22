fcc958e3741859d7f07f9bfc3879561b3a2cf41c
Ellen Graham <eg5se@virginia.edu>
2020-07-22 18:07:01 -0400

    preparing script for overnight run

--- src/Modeling/non_spatial_models.R
+++ src/Modeling/non_spatial_models.R
@@ -96,7 +96,9 @@ prepared_data <- response_time_data %>%
          impression_category,
          possible_impression_category_collapsed,
          patient_first_race_collapsed,
-         time_of_day) %>%
+         time_of_day,
+         scene_gps_latitude,
+         scene_gps_longitude) %>% # include lat and long for comparison with neighborhood data
   mutate(across(everything(), ~ifelse(is.na(.x) & !is.numeric(.x),
                                       "missing",
                                       .x))) %>% # for categorical variables simply add missing as a category
@@ -159,39 +161,45 @@ basic_model_freq_yes_interact <- prepared_data %>%
       data = .,
       family = "poisson")
 
+save(basic_model_bayes_no_interact, file = here::here("src", "Modeling", "model_objects", "basic_model_bayes_no_interact.RData"))
 save(basic_model_bayes_yes_interact, file = here::here("src", "Modeling", "model_objects", "basic_model_bayes_yes_interact.RData"))
+save(basic_model_freq_no_interact, file = here::here("src", "Modeling", "model_objects", "basic_model_freq_no_interact.RData"))
+save(basic_model_freq_yes_interact, file = here::here("src", "Modeling", "model_objects", "basic_model_freq_yes_interact.RData"))
+
+
+
 # load(here::here("src", "Modeling", "model_objects", "glm_full_no_interaction.RData"))
-
-
-#############################################################
-# Check for spatial autocorrelation in residuals
-#############################################################
-
-model_res <- residuals(basic_linear_model)
-
-modeled_data <- prepared_data %>%
-  filter(across(c(response_time_hundreths_of_minutes,
-                patient_age,
-                patient_gender,
-                response_vehicle_type_collapsed,
-                after_covid,
-                impression_category,
-                possible_impression_category_collapsed,
-                patient_first_race_collapsed,
-                time_of_day), ~!is.na(.x))) %>%
-  mutate(residuals = model_res) %>%
-  filter(!is.na(scene_gps_latitude)) %>%
-  sample_frac(0.20)
-
-
-incident_distances <- as.matrix(dist(cbind(modeled_data$scene_gps_latitude, modeled_data$scene_gps_longitude)))
-incident_distances <- 1/incident_distances
-diag(incident_distances) <- 0
-incident_distances[is.infinite(incident_distances)] <- 0
-
-
-
-ape::Moran.I(modeled_data$residuals, incident_distances)
-
-
-## There is no global spatial autocorrelation.
+#
+#
+# #############################################################
+# # Check for spatial autocorrelation in residuals
+# #############################################################
+#
+# model_res <- residuals(basic_linear_model)
+#
+# modeled_data <- prepared_data %>%
+#   filter(across(c(response_time_hundreths_of_minutes,
+#                 patient_age,
+#                 patient_gender,
+#                 response_vehicle_type_collapsed,
+#                 after_covid,
+#                 impression_category,
+#                 possible_impression_category_collapsed,
+#                 patient_first_race_collapsed,
+#                 time_of_day), ~!is.na(.x))) %>%
+#   mutate(residuals = model_res) %>%
+#   filter(!is.na(scene_gps_latitude)) %>%
+#   sample_frac(0.20)
+#
+#
+# incident_distances <- as.matrix(dist(cbind(modeled_data$scene_gps_latitude, modeled_data$scene_gps_longitude)))
+# incident_distances <- 1/incident_distances
+# diag(incident_distances) <- 0
+# incident_distances[is.infinite(incident_distances)] <- 0
+#
+#
+#
+# ape::Moran.I(modeled_data$residuals, incident_distances)
+#
+#
+# ## There is no global spatial autocorrelation.
